0fe11cb7010a88111d5d08ed48f75258f718be9a
PBhavishya10407 <72333879+PBhavishya10407@users.noreply.github.com>
2020-11-11 17:04:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Supply-Mission-02
+# SupplyMission
---
+++ Rectangle.js
@@ -0,0 +1,24 @@
+class Rectangle
+{
+    constructor(x,y,width,height)
+    {
+
+        var option=
+        {
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+
+    display()
+    {
+      var pos=this.body.position;
+      stroke("red");
+      rect(pos.x,pos.y,this.width,this.height);
+      rect(pos.x,pos.y,this.width,this.height);
+      rect(pos.x,pos.y,this.width,this.height);
+    }
+}
